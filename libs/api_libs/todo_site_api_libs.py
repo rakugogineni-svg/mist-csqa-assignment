071c18890970499e5fd5c5efb62aaa12617ff182
rakugogineni-svg <rakugogineni@gmail.com>
2026-06-12 22:23:35 -0700

    Update todo_site_api_libs.py

--- libs/api_libs/todo_site_api_libs.py
+++ libs/api_libs/todo_site_api_libs.py
@@ -1,13 +1,1 @@
-# TODO - Candidate to add Site API Libs when working on the assignment.
-Yes exactly! Do that now:
 
-Copy the code below directly from Claude
-Go to your fork: https://github.com/rakugogineni-svg/mist-csqa-assignment
-Navigate to tests/api_tests/todo_site_api_tests.py
-Click pencil icon
-Select all (Cmd+A) and delete
-Paste (Cmd+V)
-Check indentation looks correct
-Click Commit changes twice
-
-Here is the code:
